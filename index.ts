a14c6a1d1ae948cb81d881d70c83c1237d05d6a0
Shayiz <muhammedshayizkp@gmail.com>
2025-08-23 15:56:15 +0530

    updated the code

--- index.ts
+++ index.ts
@@ -2,22 +2,19 @@
 import { buildApp } from './app';
 import { ENV } from "./config/env";
 async function start() {
-  const app = await buildApp();
+  
 
   try {
+    const app = await buildApp();
     await app.listen({ port: ENV.PORT, host: '0.0.0.0' });
     console.log(`🚀 Server running on http://localhost:${ENV.PORT}`);
   } catch (err) {
-    app.log.error(err);
+    console.log(err);
     process.exit(1);
   }
 }
 
-// Call start and handle any errors
-start().catch((error) => {
-  console.error('❌ Startup failed:', error);
-  process.exit(1);
-});
+start();
 
  
 
