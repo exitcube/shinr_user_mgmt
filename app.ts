76f753be6d7ef00912d96437b279b52df5ef5cb4
Shayiz <muhammedshayizkp@gmail.com>
2025-08-24 15:55:23 +0530

    added error handling code

--- app.ts
+++ app.ts
@@ -1,14 +1,22 @@
 import Fastify from 'fastify';
 import typeormPlugin from './plugins/typeorm';
+import errorHandlerPlugin from './plugins/errorHandler';
 import rootRoute from './routes/root';
+import usersRoute from './routes/users';
 
 
 export async function buildApp() {
   const fastify = Fastify({
     logger: true,
   });
+  
+  // Register plugins
   await fastify.register(typeormPlugin);
+  await fastify.register(errorHandlerPlugin);
+  
+  // Register routes
   await fastify.register(rootRoute);
+  await fastify.register(usersRoute, { prefix: '/users' });
 
   return fastify;
 }
