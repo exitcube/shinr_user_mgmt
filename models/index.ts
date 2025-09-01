c1d14bd7133a14ebcb7f53421d44961302c07ced
Shayiz <muhammedshayizkp@gmail.com>
2025-09-01 21:10:05 +0530

    updated index and added user type

--- models/index.ts
+++ models/index.ts
@@ -1,17 +1,12 @@
-// import { User } from './User';
+import { User } from './User';
 // import { Product } from './Product';
 
 // Export all entities as an array for TypeORM configuration
 export const entities = [
-    // User,
-    // Product,
-    // Add your entities here
+    User,
+
 ];
 
-// // Export individual entities for direct imports
-// export { User } from './User';
-// export { Product } from './Product';
+// Export individual entities and types
+export { User, type User as UserType } from './User';
 
-// // Export types for TypeScript support
-// export type { User as UserType } from './User';
-// export type { Product as ProductType } from './Product';
