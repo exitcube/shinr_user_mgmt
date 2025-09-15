c42e1e4a290ca381166409b9dddf4a08648a5347
Adarsh2325 <adarsh.vinod.3762@gmail.com>
2025-09-15 17:03:56 +0530

    Merge branch 'feature/add-swagger' of https://github.com/exitcube/shinr_user_mgmt into feature/add-swagger

--- app.ts
+++ app.ts
@@ -1,44 +1,44 @@
-import Fastify from 'fastify';
-import typeormPlugin from './plugins/typeorm';
-import errorHandlerPlugin from './plugins/errorHandler';
-import rootRoute from './routes/root';
-import path from 'path';
-import fastifyStatic from '@fastify/static';
-// import usersRoute from './routes/users';
+import Fastify from "fastify";
+import typeormPlugin from "./plugins/typeorm";
+import errorHandlerPlugin from "./plugins/errorHandler";
+import rootRoute from "./routes/root";
+import path from "path";
+import fastifyStatic from "@fastify/static";
+import * as swaggerUiDist from "swagger-ui-dist";
+
 
 
 export async function buildApp() {
   const fastify = Fastify({
     logger: true,
   });
-    // Swagger UI
-    await fastify.register(fastifyStatic, {
-      root: path.join(__dirname, 'swagger'),
-      prefix: '/swagger/', // URL will be /swagger/swagger.yaml
-    });
-    
-    const swaggerUiDist = require('swagger-ui-dist');
-    const swaggerUiPath: string = swaggerUiDist.getAbsoluteFSPath();
-    await fastify.register(fastifyStatic, {
-      root: swaggerUiPath,
-      prefix: '/docs/',
-      decorateReply: false,
-    });
-    
-    // Redirect /docs to the UI with our spec
-    fastify.get('/docs', async (request, reply) => {
-      return reply.redirect('/docs/index.html?url=/swagger/swagger.yaml');
-    });
-  
+
+  // Swagger YAML spec static
+  await fastify.register(fastifyStatic, {
+    root: path.join(__dirname, "swagger"),
+    prefix: "/swagger/", // URL will be /swagger/swagger.yaml
+  });
+
+  // Swagger UI static files
+  const swaggerUiPath: string = swaggerUiDist.getAbsoluteFSPath();
+  await fastify.register(fastifyStatic, {
+    root: swaggerUiPath,
+    prefix: "/docs/",
+    decorateReply: false,
+  });
+
+  // Redirect /docs → Swagger UI with our spec
+  fastify.get("/docs", async (_request, reply) => {
+    return reply.redirect("/docs/index.html?url=/swagger/swagger.yaml");
+  });
+
   // Register plugins
   await fastify.register(typeormPlugin);
   await fastify.register(errorHandlerPlugin);
 
- 
-
   // Register routes
   await fastify.register(rootRoute);
-  // await fastify.register(usersRoute, { prefix: '/users' });
+  // await fastify.register(usersRoute, { prefix: "/users" });
 
   return fastify;
 }
---
+++ types/swagger-ui-dist.d.ts
@@ -0,0 +1,7 @@
+declare module 'swagger-ui-dist' {
+  export function getAbsoluteFSPath(): string;
+  const swaggerUiDist: {
+    getAbsoluteFSPath(): string;
+  };
+  export default swaggerUiDist;
+}
