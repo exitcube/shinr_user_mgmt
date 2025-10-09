4b75923624cbdd7e16bbe3bf284459e9cee2a184
Adarsh2325 <adarsh.vinod.3762@gmail.com>
2025-10-09 10:24:26 +0530

    Code Updated

--- plugins/user.ts
+++ plugins/user.ts
@@ -2,9 +2,11 @@ import fp from 'fastify-plugin';
 import { FastifyPluginAsync } from 'fastify';
 import { User } from '../models/User';
 import { DataSource } from 'typeorm';
+import { validate as isUUID } from 'uuid';
 
 interface UserDeviceParams {
-  userUUID: string;
+  userUUID?: string;
+  userId?: number;
   deviceUUID?: string;
 }
 
@@ -17,38 +19,38 @@ declare module 'fastify' {
 
 const userDevicePlugin: FastifyPluginAsync = async (fastify) => {
   try {
-    fastify.decorate('getUserDeviceInfo', async ({ userUUID, deviceUUID }: UserDeviceParams) => {
-      if (!userUUID) throw new Error('userUUID is required');
-
-      const userRepo = fastify.db.getRepository(User);
-
-      
-      const where: any = { uuid: userUUID, isActive: true };
-      if (deviceUUID) {
-        where.device = { uuid: deviceUUID, isActive: true };
-      }
-
-      // Fetch user along with device using relation 
-      const user = await userRepo.findOne({
-        where,
-        relations: ['device'], 
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          mobile: true,
-          lastActive: true,
-          device: {
-            id: true,
-            lastLogin: true,
-            userAgent: true,
-            ipAddress: true,
-          }
+    fastify.decorate(
+      'getUserDeviceInfo',
+      async ({ userUUID, userId, deviceUUID }: UserDeviceParams) => {
+        
+        if (!userUUID && !userId) {
+          throw new Error('Either userUUID or userId is required');
         }
-      });
 
-      return user;
-    });
+        const userRepo = fastify.db.getRepository(User);
+
+        
+        const where: any = { isActive: true };
+        if (userUUID) {
+          if (!isUUID(userUUID)) throw new Error('Invalid userUUID format');
+          where.uuid = userUUID;
+        } else if (userId) {
+          where.id = userId;
+        }
+
+        if (deviceUUID) {
+          where.device = { uuid: deviceUUID, isActive: true };
+        }
+
+        // Fetch user + device relation
+        const user = await userRepo.findOne({
+          where,
+          relations: ['device'], // fetch user and related device data
+        });        
+
+        return user;
+      }
+    );
   } catch (error: any) {
     throw new Error(error.message);
   }
