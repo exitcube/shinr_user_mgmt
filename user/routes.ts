24a7e280185dbf7edf7a14a17db1a05061da558d
Ra-zimm <razmrz666@gmail.com>
2025-10-16 12:22:52 +0530

    removing adress API

--- user/routes.ts
+++ user/routes.ts
@@ -8,4 +8,5 @@ export default async function userRoutes(fastify: FastifyInstance, opts: Fastify
     const handler = controller(fastify, opts);
     fastify.post('/get-location/reverse-geocode', { preHandler: [authValidationPreHandler, validation(reverseGeocodeValidate)] }, handler.reverseGeocodeHandler);
     fastify.post('/add-address',{preHandler: [authValidationPreHandler]},handler.addAddressHandler);
+    fastify.post('/remove-address/:id',{preHandler: [authValidationPreHandler]},handler.removeAddressHandler)
 }
